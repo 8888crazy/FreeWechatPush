2631de20c0cba8f5910e2009a9e0937b832baf59
fxx <fengxiaoxing@01tec.net>
2025-07-17 12:15:22 +0800

    支持多人推动功能

--- main.py
+++ main.py
@@ -11,7 +11,15 @@ from bs4 import BeautifulSoup
 appID = "wx1b3c649164eebf82"
 appSecret = "d9e140e1a67b3e147462cf29438af1b4"
 #收信人ID即 用户列表中的微信号，见上文
-openId = "ofOugvo0l0L1wUwdNpKwNmxrB5NQ"
+# openId = "ofOugvo0l0L1wUwdNpKwNmxrB5NQ"
+# 收信人ID列表（多个用户）
+openIds = [
+    "ofOugvo0l0L1wUwdNpKwNmxrB5NQ",  # 用户1
+    "ofOugvptBC1pSiOLgp8f5GjpfQJs",                # 用户2
+    "USER_OPEN_ID_3",                # 用户3
+    # 添加更多用户...
+]
+
 # 天气预报模板ID
 weather_template_id = "OqxHyfroYI9lryCQwR-MHukMcy9r7qhKScmw2NPfzyI"
 # 时间表模板ID
@@ -66,8 +74,11 @@ def get_weather(my_city):
 
 def get_access_token():
     # 获取access token的url
-    url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}' \
-        .format(appID.strip(), appSecret.strip())
+    # url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}' \
+    #     .format(appID.strip(), appSecret.strip())
+    # 获取access token的url
+    url = f'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={appID.strip()}&secret={appSecret.strip()}'
+
     response = requests.get(url).json()
     print(response)
     access_token = response.get('access_token')
@@ -89,54 +100,70 @@ def send_weather(access_token, weather):
     # template_id 就是模板ID
     # url 就是点击模板跳转的url
     # data就按这种格式写，time和text就是之前{{time.DATA}}中的那个time，value就是你要替换DATA的值
+    # 循环发送给所有用户
+    for openId in openIds:
 
-    import datetime
-    today = datetime.date.today()
-    today_str = today.strftime("%Y年%m月%d日")
+        import datetime
+        today = datetime.date.today()
+        today_str = today.strftime("%Y年%m月%d日")
 
-    body = {
-        "touser": openId.strip(),
-        "template_id": weather_template_id.strip(),
-        "url": "https://weixin.qq.com",
-        "data": {
-            "date": {
-                "value": today_str
-            },
-            "region": {
-                "value": weather[0]
-            },
-            "weather": {
-                "value": weather[2]
-            },
-            "temp": {
-                "value": weather[1]
-            },
-            "wind_dir": {
-                "value": weather[3]
-            },
-            "today_note": {
-                "value": get_daily_love()
+        body = {
+            "touser": openId.strip(),
+            "template_id": weather_template_id.strip(),
+            "url": "https://weixin.qq.com",
+            "data": {
+                "date": {
+                    "value": today_str
+                },
+                "region": {
+                    "value": weather[0]
+                },
+                "weather": {
+                    "value": weather[2]
+                },
+                "temp": {
+                    "value": weather[1]
+                },
+                "wind_dir": {
+                    "value": weather[3]
+                },
+                "today_note": {
+                    "value": get_daily_love()
+                }
             }
         }
-    }
-    url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}'.format(access_token)
-    print(requests.post(url, json.dumps(body)).text)
-
+        # url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}'.format(access_token)
+        # print(requests.post(url, json.dumps(body)).text)
+        url = f'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={access_token}'
+        response = requests.post(url, json.dumps(body)).json()
+        print(f"发送给 {openId} 的结果: {response}")
+        
+        # 添加延迟避免频率限制
+        time.sleep(0.5)    
 
 def send_timetable(access_token, message):
-    body = {
-        "touser": openId,
-        "template_id": timetable_template_id.strip(),
-        "url": "https://weixin.qq.com",
-        "data": {
-            "message": {
-                "value": message
-            },
+    # 循环发送给所有用户
+    for openId in openIds:
+    
+        body = {
+            # "touser": openId,
+            "touser": openId.strip(),
+            # "template_id": timetable_template_id.strip(),
+            "url": "https://weixin.qq.com",
+            "data": {
+                "message": {
+                    "value": message
+                },
+            }
         }
-    }
-    url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}'.format(access_token)
-    print(requests.post(url, json.dumps(body)).text)
-
+        # url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}'.format(access_token)
+        # print(requests.post(url, json.dumps(body)).text)
+        url = f'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={access_token}'
+        response = requests.post(url, json.dumps(body)).json()
+        print(f"发送给 {openId} 的结果: {response}")
+        
+        # 添加延迟避免频率限制
+        time.sleep(0.5)
 
 def weather_report(city):
     # 1.获取access_token
@@ -144,21 +171,23 @@ def weather_report(city):
     # 2. 获取天气
     weather = get_weather(city)
     print(f"天气信息： {weather}")
-    # 3. 发送消息
+    # 3. 发送消息给所有用户
     send_weather(access_token, weather)
 
 
 def timetable(message):
     # 1.获取access_token
     access_token = get_access_token()
-    # 3. 发送消息
+    # 3. 发送消息给所有用户
     send_timetable(access_token, message)
 
 
 if __name__ == '__main__':
+    # 发送给所有用户
     weather_report("沈阳")
     # timetable("第二教学楼十分钟后开始英语课")
-
+    
+    # 定时任务示例（需要时取消注释）
     # schedule.every().day.at("18:30").do(weather_report, "南京")
     # schedule.every().monday.at("13:50").do(timetable, "第二教学楼十分钟后开始英语课")
     #while True:
